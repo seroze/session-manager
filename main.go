bb7a89647313b04ba8d2530ef75ba2b7687c75aa
seroze kim <serozekim@gmail.com>
2025-02-15 19:13:02 +0530

    wip

--- main.go
+++ main.go
@@ -1,8 +1,9 @@
 package main 
 
 import "github.com/maddelasaikarthik/session-manager/session"
+import "fmt"
 
 func main(){
-	
+	fmt.Println("Session Manager is Running")
 	session.SayHello("John")
 }
--- session/session.go
+++ session/session.go
@@ -5,3 +5,8 @@ import "fmt"
 func sayHello(name string) {
 	fmt.Println("Hello: " name)
 }
+
+func Init(){
+	fmt.Println("Session initialized")
+}
+
